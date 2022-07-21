7310ed28506e447751066be60f32fa0cb0d98a3c
Anmol Gupta <iitkanmol438@gmail.com>
2022-07-21 20:23:58 +0530

    Setted up local passport strategy config

---
+++ config/passport_local_strategy.js
@@ -0,0 +1,42 @@
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+const User = require("../models/user");
+
+passport.use(
+  new localStrategy(
+    {
+      usernameField: "email",
+    },
+    function (email, password, done) {
+      User.findOne({ email: email }, function (err, user) {
+        if (err) {
+          console.log("Error in finding user --> passport");
+          return done(err);
+        }
+        if (!user || password != user.password) {
+          console.log("Invalid username/password");
+          return done(null, false);
+        }
+
+        return done(null, user);
+      });
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    if (err) {
+      console.log("Error in finding user --> passport");
+      return done(err);
+    }
+    return done(null, user);
+  });
+});
+
+
+module.exports = passport;
