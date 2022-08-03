763a65021bd1434ec7507fa98d4a26ef02a85df8
Anmol Gupta <iitkanmol438@gmail.com>
2022-08-04 00:15:36 +0530

    Delete posts via api without authorisation and authentication

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,47 @@
-module.exports.index = function(req, res){
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function (req, res) {
+
+
+    let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate("user")
+        .populate({
+            path: "comments",
+            populate: {
+                path: "user",
+            },
+        });
+
+
     return res.status(200).json({
-        message:"Post List",
-        posts: []
+        message: "Post List",
+        posts: posts
     });
+};
+
+
+module.exports.destroy = async function (req, res) {
+
+    try {
+        let post = await Post.findById(req.params.id);
+
+        await Comment.deleteMany({ _id: post.comments });
+        post.remove();
+        return res.status(200).json({
+            message:"Post and its comments deleted",
+        })
+        // }else{
+        //     console.log('not authorised');
+        //     return res.redirect('back');
+        // }
+    } catch (err) {
+        console.log("Error --->  ", err);
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
+
+    }
+
 };
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const post_api = require('../../../controllers/api/v1/posts_api');
 
 router.get('/', post_api.index);
+router.delete('/:id', post_api.destroy)
 
 module.exports = router;
